80fd78e859c7a6e424522f102c862910f5f4657b
John Beck <ausonny@gmail.com>
2020-01-28 13:48:28 -0600

    version testing

--- main.js
+++ main.js
@@ -2723,7 +2723,7 @@ window.setInterval(function () {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
       if (xhr.readyState == 4 && xhr.status == 200) {
-        document.getElementById('result').innerHTML = xhr.responseText;
+        document.getElementById('#result').innerHTML = xhr.responseText;
       }
     }
     xhr.open('GET', 'version.txt');
