493771160bfc01b087d6b4242f89441c5aab1024
John Beck <ausonny@gmail.com>
2022-07-28 08:25:04 -0500

    Delete automation.js

--- built/automation.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-class Automation {
-    constructor(item, value) {
-        this.item = item;
-        this.value = value;
-    }
-}
-//# sourceMappingURL=automation.js.map
